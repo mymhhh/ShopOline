ec08b6adae8efa228990911d6a39ad1e87c0776c
mym <3216187300@qq.com>
2023-11-07 16:41:15 +0800

    环境搭建

---
+++ src/main/java/com/shop/shoponline/ShopOnlineApplication.java
@@ -0,0 +1,16 @@
+package com.shop.shoponline;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+
+public class ShopOnlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopOnlineApplication.class, args);
+	}
+
+}
